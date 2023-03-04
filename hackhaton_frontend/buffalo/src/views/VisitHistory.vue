fbac4e3e13fc766fc135e05450957ab9d4a05e14
Felipe Gebara <feagmoreira@hotmail.com>
2023-03-04 13:24:59 -0700

    adding carrousel

--- hackhaton_frontend/buffalo/src/views/VisitHistory.vue
+++ hackhaton_frontend/buffalo/src/views/VisitHistory.vue
@@ -1,15 +1,23 @@
 <template>
-  <div v-for="item in listItems" :key="item.id">
-    {{ item.title }}
-  </div>
+  <Carousel :items-to-show="2.5" :wrap-around="true">
+    <Slide v-for="item in listItems" :key="item.id">
+      <div class="carousel__item">{{ item.title }}</div>
+    </Slide>
+
+    <template #addons>
+      <Navigation />
+      <Pagination />
+    </template>
+  </Carousel>
 </template>
 
-
 <script>
+import 'vue3-carousel/dist/carousel.css'
+import { Carousel, Slide, Pagination, Navigation } from 'vue3-carousel'
 export default {
   data() {
     return {
-      listItems: this.getData()
+      listItems: this.getData(),
     }
   },
   methods: {
@@ -18,15 +26,36 @@ export default {
       const finalRes = await res.json();
       this.listItems = finalRes;
     }
-  }
+  },
+  components: {
+    Carousel,
+    Slide,
+    Pagination,
+    Navigation,
+  },
 }
 </script>
+
 <style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
+.carousel__item {
+  min-height: 200px;
+  width: 100%;
+  background-color: green;
+  color: white;
+  font-size: 20px;
+  border-radius: 8px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.carousel__slide {
+  padding: 10px;
+}
+
+.carousel__prev,
+.carousel__next {
+  box-sizing: content-box;
 }
 </style>
+

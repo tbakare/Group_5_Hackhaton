5ca60e667143e109eb0446beb65fe9873b2f4616
Michael Metz <6386318+m-metz@users.noreply.github.com>
2023-03-04 15:00:04 -0700

    New List View

--- hackhaton_frontend/buffalo/src/views/NewVisitView.vue
+++ hackhaton_frontend/buffalo/src/views/NewVisitView.vue
@@ -2,21 +2,36 @@
   <div>
     <form @submit.prevent="submitForm" v-if="!formSubmitted">
       <h1>Sexual History</h1>
-      <input type="radio" v-model="gender" value="i-have-symptoms" />
+      <input type="radio" value="i-have-symptoms" />
       <label>I have symptoms of an STI</label><br>
-      <input type="radio" v-model="gender" value="partner-has-symptoms" />
+      <input type="radio" value="partner-has-symptoms" />
       <label>My sex partner has an STI</label><br>
-      <input type="radio" v-model="gender" value="partner-has-symptoms" />
+      <input type="radio" value="partner-has-symptoms" />
       <label>I just want testing</label><br>
 
-      <input v-model="name" type="text" placeholder="Enter your name" /><br>
-      <span>Email</span><br>
-      <input v-model="email" type="email" placeholder="Enter your email" /><br>
-      <label>Gender</label><br>
+      <h2>What symptoms are you having?</h2>
+      <input type="text" placeholder="" /><br>
+      <h2>Number of sex partners</h2>
+      <h3>In the past 2 months</h3>
+      <input type="text" placeholder="" />
+      <h3>In the past 12 months</h3>
+      <input type="number" placeholder="" /><br>
 
-      <input type="radio" v-model="gender" value="Female" />
-      <label>Female</label><br>
+      <h2>Gender of sex partners</h2>
+      <input type="radio" value="Male" />
+      <label>Male</label>
+      <input type="radio" value="Female" />
+      <label>Female</label>
+      <input type="radio" value="Both" />
+      <label>Both</label><br>
+
+      <h2>Have you ever had syphilis?</h2>
+      <input type="radio" value="Yes" />
+      <label>Yes</label>
+      <input type="radio" value="No" />
+      <label>No</label><br>
       <input class="submit" type="submit" value="Submit">
+
     </form>
     <div v-if="formSubmitted">
       <h3>Form Submitted</h3>
@@ -57,7 +72,12 @@ input {
   margin: 4px;
 }
 
-span {
+input[type='radio'] {
+  /* padding: 8px 8px; */
+  margin: 10px;
+}
+
+h2 {
   font-size: 18px;
   margin: 4px;
   font-weight: 500;

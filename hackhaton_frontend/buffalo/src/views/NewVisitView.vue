3307868a67d293e8a5d0693a9029588cb7b18340
Michael Metz <6386318+m-metz@users.noreply.github.com>
2023-03-04 14:26:29 -0700

    Partial form on new Visit

--- hackhaton_frontend/buffalo/src/views/NewVisitView.vue
+++ hackhaton_frontend/buffalo/src/views/NewVisitView.vue
@@ -1,15 +1,76 @@
 <template>
-  <div class="new-visit">
-    <h1>This is the new visit page</h1>
+  <div>
+    <form @submit.prevent="submitForm" v-if="!formSubmitted">
+      <h1>Sexual History</h1>
+      <input type="radio" v-model="gender" value="i-have-symptoms" />
+      <label>I have symptoms of an STI</label><br>
+      <input type="radio" v-model="gender" value="partner-has-symptoms" />
+      <label>My sex partner has an STI</label><br>
+      <input type="radio" v-model="gender" value="partner-has-symptoms" />
+      <label>I just want testing</label><br>
+
+      <input v-model="name" type="text" placeholder="Enter your name" /><br>
+      <span>Email</span><br>
+      <input v-model="email" type="email" placeholder="Enter your email" /><br>
+      <label>Gender</label><br>
+
+      <input type="radio" v-model="gender" value="Female" />
+      <label>Female</label><br>
+      <input class="submit" type="submit" value="Submit">
+    </form>
+    <div v-if="formSubmitted">
+      <h3>Form Submitted</h3>
+      <p>Name: {{ name }}</p>
+      <p>Email: {{ email }}</p>
+      <p>Gender: {{ gender }}</p>
+      <small>Click on run to launch the app again.</small>
+    </div>
   </div>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      name: "",
+      email: "",
+      gender: "",
+      formSubmitted: false
+    };
+  },
+  methods: {
+    submitForm: function () {
+      this.formSubmitted = true
+    }
+  },
+};
+</script>
 <style>
-/* @media (min-width: 1024px) {
-  .new-visit {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
+form {
+  padding: 10px;
+  /* border: 2px solid black; */
+  border-radius: 5px;
+}
+
+input {
+  padding: 4px 8px;
+  margin: 4px;
+}
+
+span {
+  font-size: 18px;
+  margin: 4px;
+  font-weight: 500;
+}
+
+/* .submit {
+  font-size: 15px;
+  color: #fff;
+  background: #222;
+  padding: 6px 12px;
+  border: none;
+  margin-top: 8px;
+  cursor: pointer;
+  border-radius: 5px;
 } */
 </style>

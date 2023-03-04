c650cac112cbbbcd9c241fbeb1d3caea03d6a635
Felipe Gebara <feagmoreira@hotmail.com>
2023-03-04 14:56:49 -0700

    visit info incomplete page

--- hackhaton_frontend/buffalo/src/views/VisitInfo.vue
+++ hackhaton_frontend/buffalo/src/views/VisitInfo.vue
@@ -1,61 +1,29 @@
 <template>
-  <Carousel :items-to-show="2.5" :wrap-around="true">
-    <Slide v-for="item in listItems" :key="item.id">
-      <div class="carousel__item">{{ item.title }}</div>
-    </Slide>
-
-    <template #addons>
-      <Navigation />
-      <Pagination />
-    </template>
-  </Carousel>
+  {{ visit.name }} {{ visit.date }}
+  <div>
+    <button @click="this.orderStatus()">Order Status</button>
+  </div>
 </template>
 
 <script>
-import 'vue3-carousel/dist/carousel.css'
-import { Carousel, Slide, Pagination, Navigation } from 'vue3-carousel'
 export default {
   data() {
     return {
-      listItems: this.getData(),
+      visit: { "date": "03-04-23", "name": "Visit 1", "id": "1", "order": { "number": "1", "status": "processed" } },
     }
   },
   methods: {
-    async getData() {
+    async getVisitData() {
       const res = await fetch("https://jsonplaceholder.typicode.com/posts");
       const finalRes = await res.json();
-      this.listItems = finalRes;
+      this.visit = finalRes;
+    },
+    oderStatus() {
+      alert(`${this.visit.order.status}`)
     }
-  },
-  components: {
-    Carousel,
-    Slide,
-    Pagination,
-    Navigation,
-  },
+  }
 }
 </script>
 
-<style>
-.carousel__item {
-  min-height: 200px;
-  width: 100%;
-  background-color: green;
-  color: white;
-  font-size: 20px;
-  border-radius: 8px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-
-.carousel__slide {
-  padding: 10px;
-}
-
-.carousel__prev,
-.carousel__next {
-  box-sizing: content-box;
-}
-</style>
+<style></style>
 

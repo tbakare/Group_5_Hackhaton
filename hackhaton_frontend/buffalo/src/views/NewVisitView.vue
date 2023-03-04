7bce682fd210670eecfa1936f1985a374a2e3ef9
Michael Metz <6386318+m-metz@users.noreply.github.com>
2023-03-04 15:30:05 -0700

    Forgot to check in rest of form

--- hackhaton_frontend/buffalo/src/views/NewVisitView.vue
+++ hackhaton_frontend/buffalo/src/views/NewVisitView.vue
@@ -2,11 +2,11 @@
   <div>
     <form @submit.prevent="submitForm" v-if="!formSubmitted">
       <h1>Sexual History</h1>
-      <input type="radio" value="i-have-symptoms" />
+      <input type="radio" name="sh" value="i-have-symptoms" />
       <label>I have symptoms of an STI</label><br>
-      <input type="radio" value="partner-has-symptoms" />
+      <input type="radio" name="sh" value="partner-has-symptoms" />
       <label>My sex partner has an STI</label><br>
-      <input type="radio" value="partner-has-symptoms" />
+      <input type="radio" name="sh" value="partner-has-symptoms" />
       <label>I just want testing</label><br>
 
       <h2>What symptoms are you having?</h2>
@@ -18,27 +18,40 @@
       <input type="number" placeholder="" /><br>
 
       <h2>Gender of sex partners</h2>
-      <input type="radio" value="Male" />
+      <input type="radio" name="gender" value="Male" />
       <label>Male</label>
-      <input type="radio" value="Female" />
+      <input type="radio" name="gender" value="Female" />
       <label>Female</label>
-      <input type="radio" value="Both" />
+      <input type="radio" name="gender" value="Both" />
       <label>Both</label><br>
 
-      <h2>Have you ever had syphilis?</h2>
-      <input type="radio" value="Yes" />
+      <h2>Have you ever been diagnosed with HIV?</h2>
+      <input type="radio" name="hiv" value="Yes" />
       <label>Yes</label>
-      <input type="radio" value="No" />
+      <input type="radio" name="hiv" value="No" />
       <label>No</label><br>
+
+      <h2>Have you ever had syphilis?</h2>
+      <input type="radio" name="syphilis" value="Yes" />
+      <label>Yes</label>
+      <input type="radio" name="syphilis" value="No" />
+      <label>No</label><br>
+
+      <h2>Which testing would you like to order?</h2>
+      <input type="radio" name="order" value="All" />
+      <label>All</label><br>
+      <input type="radio" name="order" value="HIV and Syphilis Only" />
+      <label>HIV and Syphilis Only</label><br>
+      <input type="radio" name="order" value="Chlamydia and Gonorrhea Only" />
+      <label>Chlamydia and Gonorrhea Only</label><br>
       <input class="submit" type="submit" value="Submit">
 
     </form>
     <div v-if="formSubmitted">
-      <h3>Form Submitted</h3>
-      <p>Name: {{ name }}</p>
+      <h3>Order Sent</h3>
+      <!-- <p>Name: {{ name }}</p>
       <p>Email: {{ email }}</p>
-      <p>Gender: {{ gender }}</p>
-      <small>Click on run to launch the app again.</small>
+      <p>Gender: {{ gender }}</p> -->
     </div>
   </div>
 </template>
